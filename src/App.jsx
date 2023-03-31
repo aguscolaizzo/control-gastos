1f204b3edb7a28d5f3907f7df4a079d36a1360eb
aguscolaizzo <52088799+aguscolaizzo@users.noreply.github.com>
2023-03-31 09:20:37 +0200

    Filtro de gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import ListadoGastos from './components/ListadoGastos';
+import { Filtros } from './components/Filtros';
 import Modal from './components/Modal';
 import { generarID } from './helpers';
 import { formatearFecha } from './helpers';
@@ -8,14 +9,17 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
-  const [presupuesto, setPresupuesto] = useState("");
+  const [presupuesto, setPresupuesto] = useState(Number(localStorage.getItem('presupuesto') ?? 0));
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
   
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
-  const [gastos, setGastos] = useState([])
+  const [gastos, setGastos] = useState(localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []);
 
   const [gastoEditar, setGastoEditar] = useState({})
+
+  const [filtro, setFiltro] = useState("")
+  const [gastosFiltrados, setGastosFiltrados] = useState([])
 
   useEffect(() => {
     if(Object.keys(gastoEditar).length > 0){
@@ -27,6 +31,35 @@ function App() {
       }, 500 );
     }
   }, [ gastoEditar ])
+
+  useEffect(() => {
+    localStorage.setItem('presupuesto', presupuesto ?? 0)
+  }, [ presupuesto ])
+
+  useEffect(() => {
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])
+  }, [ gastos ])
+
+  useEffect(() => {
+    if(filtro){
+
+      const gastosFiltrados = gastos.filter( gasto => gasto.categoria === filtro)
+      setGastosFiltrados(gastosFiltrados)
+
+    }
+    
+
+
+  }, [ filtro ])
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+    if(presupuestoLS > 0) {
+      setIsValidPresupuesto(true)}
+
+  }, [])
+
 
   const handleNuevoGasto = () => {
     setModal(true)
@@ -85,10 +118,16 @@ function App() {
       {isValidPresupuesto && (
         <>
               <main>
+                <Filtros
+                filtro={filtro}
+                setFiltro={setFiltro}
+                />
                 <ListadoGastos
                 gastos={gastos}
                 setGastoEditar={setGastoEditar}
                 eliminarGasto={eliminarGasto}
+                filtro={filtro}
+                gastosFiltrados={gastosFiltrados}
                 />
               </main>
               <div className='nuevo-gasto'>
--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -2,19 +2,46 @@ import {useState} from 'react'
 import Gasto from './Gasto'
 
 
-const ListadoGastos = ({gastos, setGastoEditar, eliminarGasto}) => {
+const ListadoGastos = ({gastos, setGastoEditar, eliminarGasto, filtro, gastosFiltrados}) => {
   return (
     <div className='listado-gastos contenedor'>
-        <h2>{gastos.length ? "Hay gastos" : "No hay gastos aun"}</h2>
-        {gastos.map( gasto => (
-             
-             <Gasto 
+        
+        
+        {
+          
+          filtro ? 
+          (
+          <>
+            <h2>{gastosFiltrados.length ? "Gastos Filtrados" : "No Podemos Filtar los Gastos"}</h2>
+            { gastosFiltrados.map( gasto => (
+              
+              <Gasto 
                 key={gasto.id+1}
                 gasto={gasto}
                 setGastoEditar={setGastoEditar}
                 eliminarGasto={eliminarGasto}
-             />
-        ))}
+              />)) 
+            } 
+          </>
+       
+       )
+       
+       :
+       
+       <>
+          <h2>{gastos.length ? "Todos los Gastos" : "No hay gastos aun"}</h2>          
+          { gastos.map( gasto => (
+            
+            <Gasto 
+              key={gasto.id+1}
+              gasto={gasto}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />)) 
+          } 
+      </>
+
+       }
     </div>
   )
 }
